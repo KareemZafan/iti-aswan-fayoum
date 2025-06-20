9b1a9baf446b6f640c8e5afb580161c7cf6af617
KareemZafan <kareemm.zafan@gmail.com>
2025-06-20 11:40:11 +0300

    Add getMax function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int getMax(int a , int b){
+
+ return (a > b) ? a : b; 
+}
+
+
 int main(void){
 
  printf("Hello World");
+ 
+ printf("Max of(%d,%d) is %d",a,b,getMax(a,b)); 
 }
