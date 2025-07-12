e1e56d37719b1f682f9cfce6c72f8da0373c574a
KareemZafan <kareemm.zafan@gmail.com>
2025-07-12 09:56:18 +0300

    Adding SQRT function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 
 int getMax(int a , int b){
 
  return (a > b) ? a : b; 
+}
+
+double getSquareRoot(double a){
+ return sqrt(a); 
 }
 
 
@@ -11,4 +16,7 @@ int main(void){
  printf("Hello World");
  
  printf("Max of(%d,%d) is %d",a,b,getMax(a,b)); 
+ printf("Square root of(%d) is %d",100,getSquareRoot(100)); 
+ 
+ 
 }
