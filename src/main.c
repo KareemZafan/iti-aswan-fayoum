8823f5923d7e5193cef09a769947dbeadb29f9bd
KareemZafan <kareemm.zafan@gmail.com>
2025-07-12 12:50:31 +0300

    adding abs fuction

--- src/main.c
+++ src/main.c
@@ -10,6 +10,11 @@ double getSquareRoot(double a){
  return sqrt(a); 
 }
 
+int abs(int a){
+return a > 0 ? a : -a; 
+
+}
+
 
 int main(void){
 
@@ -17,6 +22,8 @@ int main(void){
  
  printf("Max of(%d,%d) is %d",100,200,getMax(100,200)); // 200 
  printf("Square root of(%f) is %0.2f",100,getSquareRoot(100)); //10.00
+ 
+ printf("Absolute value of(%d) is %d",-20,abs(-20)); // 20
  
  
 }
